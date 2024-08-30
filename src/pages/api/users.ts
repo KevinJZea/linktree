bd99b42486f9a608d96a0e5baf2fdab7acc49a8a
kevinjzea <kevinjzea@gmail.com>
2024-08-29 21:48:11 -0600

    ➕ feat: Added users data and profile images

---
+++ src/pages/api/users.ts
@@ -0,0 +1,20 @@
+import type { APIRoute } from "astro";
+import users from "../../data/users.json";
+
+export const prerender = false;
+
+export const GET: APIRoute = async () => {
+  return Response.json(users);
+}
+
+export const POST: APIRoute = async ({ request }) => {
+  const { slug } = await request.json();
+
+  if (!slug) return Response.json(users);
+
+  const userData = users.find((user) => user.username === slug);
+
+  if (!userData) return Response.json({ message: "User data not found" });
+
+  return Response.json(userData);
+};
